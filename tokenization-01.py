a3715a23bb33989f59ba59f1f47f0d17dfa4fad9
Rafael Almeida <rafael.profeng@gmail.com>
2025-11-28 18:39:04 -0300

    feat: Add first tokenization approach

---
+++ tokenization-01.py
@@ -0,0 +1,34 @@
+import nltk
+
+nltk.download("punkt_tab")
+
+text = """
+Abacaxi com leite em pó é uma das melhores sobremesas e menos calóricas considerando o sabor que se pode comer após o almoço.
+Com frequência o ser humano aproveita a oportunidade de, após o almoço, degustar algum tipo de alimento doce.
+Acontece que durante a dieta de perda de peso, essa oportunidade deve ser saborada com cautela.
+A escolha de frutas em detrimento a alimentos ultraprocessados ou industrializados mostra-se rica em diversos aspectos.
+"""
+
+word_tokens = nltk.word_tokenize(text)
+sentence_tokens = nltk.sent_tokenize(text)
+print("######### WORD TOKENS ############")
+print(word_tokens)
+print("######### SENTENCE TOKENS ############")
+print(sentence_tokens)
+
+### Preprocessing step -> removing special chars, creating standard
+
+
+def preprocess(text):
+    tokens = nltk.word_tokenize(text.lower())
+    return [word for word in tokens if word.isalnum()]
+
+
+documents = [
+    "Machine learning é o aprendizado automático de máquinas a partir de dados.",
+    "Ele permite que sistemas façam previsões e decisões sem programação explícita.",
+    "É usado em áreas como reconhecimento de voz, imagens e recomendação de conteúdo.",
+]
+
+preprocessed_docs = [" ".join(preprocess(doc)) for doc in documents]
+print(preprocessed_docs)
