8f051a61309a957ae34893e7f082104537c66c41
Rafael Almeida <rafael.profeng@gmail.com>
2025-12-04 09:55:41 -0300

    feat: enhance TF-IDF processing and update document examples

--- tokenization-02.py
+++ tokenization-02.py
@@ -1,4 +1,5 @@
 import nltk
+from numpy import number
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -17,7 +18,11 @@ documents = [
     "Machine Learning é legal",
     "Aprender sobre machine learning pode ser essencial",
     "Jamais se viu uma revolução como aquela que a IA está promovendo",
-    "Conta-se que mesmo a internet não impactou o mundo como o ML",
+    "Conta-se que mesmo a internet não impactou o mundo como a IA",
+    "Machine learning é um dos campos que está incluso em inteligência artificial",
+    "Em adição a isso, pode-se citar natural language processing, robotics, deep learning, dentre outros",
+    "Machine learning é uma área da inteligência artificial que se concentra em formas de gerar aprendizado à um computador",
+    "Deep learning é a ciência cujo objeto de estudo é o aprendizado profundo",
 ]
 # Preprocessing step
 preprocessed_docs = perform_preprocess_and_join(documents)
@@ -25,14 +30,19 @@ preprocessed_docs = perform_preprocess_and_join(documents)
 vectorizer = TfidfVectorizer()
 # Creating Vector Space Model for the documents
 tfidf_matrix = vectorizer.fit_transform(preprocessed_docs)
+query = "o que é machine learning"
+
 
 # Now we create a vector to represent the query in that n-dimension space
 # where the documents live
-query = "machine learning"
-query_vector = vectorizer.transform([query])
-print(preprocessed_docs)
-print(tfidf_matrix)
-print(query_vector)
-# Calculating relevance from document vectors
-similarity = cosine_similarity(tfidf_matrix, query_vector).flatten()
-print(similarity)
+def generate_tfidf(query: str, vectorizer: TfidfVectorizer, tfidf_matrix):
+    query_vector = vectorizer.transform([query])
+    # Calculating relevance from document vectors
+    similarities = cosine_similarity(tfidf_matrix, query_vector).flatten()
+    return sorted(list(enumerate(similarities)), key=lambda x: x[1], reverse=True)
+
+
+similarities_ordered = generate_tfidf(query, vectorizer, tfidf_matrix)
+print("########## TOP 5 Most Similar documents to the query ##########")
+for index, similarity in similarities_ordered[:5]:
+    print(index, similarity)
